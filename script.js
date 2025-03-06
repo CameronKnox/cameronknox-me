6584273326ad2d960c15a12b0622f74abf9c004a
CameronKnox <cbknox13@gmail.com>
2025-03-05 20:33:12 -0500

    new entry 3-5

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Function to load and style text content in iframes
 function loadAndStyleIframe(iframe, filePath) {
-    fetch(filePath)
-        .then((response) => response.text())
-        .then((data) => {
-            // Create a styled HTML structure for the content
-            const styledContent = `
+  fetch(filePath)
+    .then((response) => response.text())
+    .then((data) => {
+      // Create a styled HTML structure for the content
+      const styledContent = `
           <!DOCTYPE html>
           <html>
             <head>
@@ -21,25 +21,27 @@ function loadAndStyleIframe(iframe, filePath) {
           </html>
         `;
 
-            // Write the styled content to the iframe
-            const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-            iframeDoc.open();
-            iframeDoc.write(styledContent);
-            iframeDoc.close();
+      // Write the styled content to the iframe
+      const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+      iframeDoc.open();
+      iframeDoc.write(styledContent);
+      iframeDoc.close();
 
-            // Resize the iframe to fit its content
-            iframe.style.height = iframeDoc.body.scrollHeight + 'px';
-        })
-        .catch((error) => {
-            console.error('Error loading file:', error);
-        });
+      // Resize the iframe to fit its content
+      iframe.style.height = iframeDoc.body.scrollHeight + 'px';
+    })
+    .catch((error) => {
+      console.error('Error loading file:', error);
+    });
 }
 
 // Load and style iframes when the page loads
 window.onload = function () {
-    const iframe1 = document.getElementById('blog1');
-    const iframe2 = document.getElementById('blog2');
+  const iframe1 = document.getElementById('blog1');
+  const iframe2 = document.getElementById('blog2');
+  const iframe3 = document.getElementById('blog3');
 
-    loadAndStyleIframe(iframe1, './blogs/Blog_1-23-2025.txt');
-    loadAndStyleIframe(iframe2, './blogs/current-setups.txt');
+  loadAndStyleIframe(iframe1, './blogs/Blog_1-23-2025.txt');
+  loadAndStyleIframe(iframe2, './blogs/current-setups.txt');
+  loadAndStyleIframe(iframe3, './blogs/Blog_3-5-2025.txt');
 };
